8411c5ee6a9862670b0813e24d0ff4470fdd7165
nasrinsinu6-creator <nasrinsinu6@gmail.com>
2026-03-18 14:54:13 +0530

    completed

---
+++ r13/Tempconv.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Tempconv{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+while(true){
+System.out.println("---Temperature converter---");
+System.out.println("1.Celsius to Fahrenheit");
+System.out.println("2.Fahrenheit to Celsius");
+System.out.println("3. Exit");
+System.out.println("Enter your option");
+int choice = sc.nextInt();
+sc.nextLine();
+ if(choice == 3){
+   	System.out.println("Execution terminated");
+   	break;
+   }
+System.out.println("Enter temperature :");
+String temp=sc.nextLine();
+try{
+double temperature=Double.parseDouble(temp);
+double convertedTemp;
+switch(choice){
+case 1:
+convertedTemp=(temperature * 9/5) + 32;
+System.out.printf("Temperature %.2f C is equal to %.2f F\n", temperature, convertedTemp);
+break;
+case 2:
+convertedTemp=(temperature - 32) * 5/9;
+System.out.printf("Temperature %.2f F is equal to %.2f C\n", temperature, convertedTemp);
+break;
+default:
+System.out.println("Error:Invalid menu selection");
+break;
+}
+}catch(NumberFormatException e){
+System.out.println("Error:Invalid input.you must enter a numerical value.");
+}
+}
+sc.close();
+}
+}
+
+
+
